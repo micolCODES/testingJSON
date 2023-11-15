5b256ddee6b1ef6ef514ad1dd96b0a3861ca1072
micolCODES <micolromano124@gmail.com>
2023-11-15 17:34:35 +0100

    Initial Commit

---
+++ testingJSON/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testingJSON
+//
+//  Created by Micol on 15.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testingJSON/testingJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingJSONApp.swift
+//  testingJSON
+//
+//  Created by Micol on 15.11.23.
+//
+
+import SwiftUI
+
+@main
+struct testingJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
