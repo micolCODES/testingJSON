b8c7645bcd3782e74ffaaa7e05c1b13bf819ca88
micolCODES <micolromano124@gmail.com>
2023-11-15 18:22:41 +0100

    add test setup

--- testingJSON/ContentView.swift
+++ testingJSON/ContentView.swift
@@ -12,8 +12,21 @@ struct ContentView: View {
     let meridians: [String: Meridian] = Bundle.main.decode("meridiansNacupoints.json")
     
     var body: some View {
-        VStack {
-            Text("\(meridians.count)")
+        VStack(alignment: .leading){
+            Text("Total meridians: \(meridians.count)") // "12"
+            Text("First meridian: ") // "Liver"
+            Text("Acupoints on meridian: ") // liver.points.count -> "6"
+            Text("Element of meridian: ") // "wood"
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Level 2 point: ")
+                }
+                VStack(alignment: .trailing) {
+                    Text("-") // name -> "Liv-2"
+                    Text("-") // element -> "fire"
+                    Text("-") // description -> "Description"
+                }
+            }
         }
         .padding()
     }
