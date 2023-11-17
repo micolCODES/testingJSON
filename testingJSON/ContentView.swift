49185724c8d02db067b9159e8053d6b18fa18b0d
micolCODES <micolromano124@gmail.com>
2023-11-17 11:25:21 +0100

    update to NavigationLink only view

--- testingJSON/ContentView.swift
+++ testingJSON/ContentView.swift
@@ -9,44 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let meridians: [String: Meridian] = Bundle.main.decode("meridiansNacupoints.json")
+//    @State private var meridians: [String: Meridian] = Bundle.main.decode("meridiansNacupoints.json")
     @State private var current = "lung"
-    @State private var level = 0
     
     let listOfMeridians = ["liver","gallbladder","heart","small intestine","pericardium","triple heater","spleen","stomach","lung","large intestine","kidney","bladder"]
     
     var body: some View {
-        let currentMeridian = meridians[current, default: Meridian(id: "Not Known", element: "Unknown", yinYang: "yinYang", points: [])]
-        
-        Form {
-            Picker("Type of Meridian", selection: $current){
-                ForEach(listOfMeridians, id: \.self) { meridian in
+        NavigationStack {
+            ForEach(listOfMeridians, id: \.self) { meridian in
+                NavigationLink {
+                    MeridianInfoView(chosenMeridian: meridian)
+                } label: {
                     Text("\(meridian)")
                 }
             }
-            VStack(alignment: .leading){
-                
-                Text("Total meridians: \(meridians.count)") // "12"
-                Text("First meridian: \(currentMeridian.id)") // "Liver"
-                Text("Acupoints on meridian: \(currentMeridian.points.count)") // liver.points.count -> "6"
-                Text("Element of meridian: \(currentMeridian.element)") // "wood"
-            }
-            Picker("Level", selection: $current){
-                ForEach(0...5, id: \.self) { level in
-                    Text(level != 0 ? "\(level)" : "master")
-                }
-            }
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(level != 0 ? "Level \(level) point" : "Master point")
-                }
-                VStack(alignment: .trailing) {
-                    Text(currentMeridian.points[level].id) // name -> "Liv-2"
-                    Text(currentMeridian.points[level].element) // element -> "fire"
-                    Text(currentMeridian.points[level].description) // description -> "Description"
-                }
-            }
         }
+//            VStack(alignment: .leading){
+//                
+//                Text("Total meridians: \(meridians.count)") // "12"
+//                Text("First meridian: \(currentMeridian.id)") // "Liver"
+//                Text("Acupoints on meridian: \(currentMeridian.points.count)") // liver.points.count -> "6"
+//                Text("Element of meridian: \(currentMeridian.element)") // "wood"
+//            }
+//            Picker("Level", selection: $current){
+//                ForEach(0...5, id: \.self) { level in
+//                    Text(level != 0 ? "\(level)" : "master")
+//                }
+//            }
+//            HStack {
+//                VStack(alignment: .leading) {
+//                    Text(level != 0 ? "Level \(level) point" : "Master point")
+//                }
+//                VStack(alignment: .trailing) {
+//                    Text(currentMeridian.points[level].id) // name -> "Liv-2"
+//                    Text(currentMeridian.points[level].element) // element -> "fire"
+//                    Text(currentMeridian.points[level].description) // description -> "Description"
+//                }
+//            }
         .padding()
     }
 }
