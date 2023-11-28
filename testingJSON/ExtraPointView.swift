54fcfa98c1708ade1a1a43f46f7c819056adacec
micolCODES <micolromano124@gmail.com>
2023-11-28 16:34:09 +0100

    add ExtraPointView

---
+++ testingJSON/ExtraPointView.swift
@@ -0,0 +1,20 @@
+//
+//  ExtraPointView.swift
+//  testingJSON
+//
+//  Created by Micol on 27.11.23.
+//
+
+import SwiftUI
+
+struct ExtraPointView: View {
+    var pointPassed: ExtraPoint
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExtraPointView()
+}
