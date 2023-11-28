4d994446b4a6ee134e354b1cb6b19155dbf6b057
micolCODES <micolromano124@gmail.com>
2023-11-28 16:33:57 +0100

    add ExtraPoint Struct

---
+++ testingJSON/ExtraPoint.swift
@@ -0,0 +1,14 @@
+//
+//  ExtraPoint.swift
+//  testingJSON
+//
+//  Created by Micol on 19.11.23.
+//
+
+import Foundation
+
+struct ExtraPoint: Codable, Identifiable {
+    var id: String
+    var target: String
+    var description: String
+}
